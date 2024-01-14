651892b92c94ade20ff8198d34dbbd18c72004d5
julioaraujo139 <jaraujo.byupathway@outlook.com>
2024-01-13 23:22:27 -0300

    commit

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,8 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Julio");
+        string color = "blue";
+        Console.WriteLine($"I am {color}");
     }
 }
